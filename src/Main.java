698aa12b8404707581f2f38002f88990ac3af68e
yatu360 <yatu360@gmail.com>
2021-04-09 02:09:50 +0100

    Simple question time game

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-import fraction.Fraction;
+
 import fraction.FractionImpl;
 
 public class Main {
@@ -56,13 +56,52 @@ public class Main {
         }
     }
 
+
+    public static void questionThree(){
+        Random rand = new Random();
+        int m = rand.nextInt(25);
+        int r = rand.nextInt(25);
+        String text;
+
+        FractionImpl Marco = new FractionImpl(m, 100);
+        FractionImpl Robert = new FractionImpl(r, 100);
+        int answer = Marco.compareTo(Robert);
+
+        System.out.println("Two brothers, Marco and Robert are to inherit a vast estate from their parents, Marco is due to" +
+                "receive "+Marco.toString()+" as a percentage fraction of the estate and Robert is due to receive "+
+                Robert.toString()+" as a percentage fraction of the estate, who is the favourite child? (Marco, Robert or Both)");
+
+        Scanner user = new Scanner(System.in);
+        String userAnswer = user.nextLine().toLowerCase(Locale.ROOT).trim();
+
+        if (answer>0 && userAnswer.equals("marco")){
+            System.out.println("Correct Answer");
+        }else if (answer<0 && userAnswer.equals("robert")){
+            System.out.println("Correct Answer");
+        }
+        else if (answer==0 && userAnswer.equals("both")){
+            System.out.println("Correct Answer");
+        }
+        else{
+            if(answer>0) text = "Marco";
+            else if (answer<0) text = "Robert";
+            else text="Both";
+            System.out.println("Incorrect answer, the correct answer is: "+text);
+        }
+
+    }
+
+
     public static void main(String[] args) {
+        System.out.println("Enter values between 1-3 to play fraction question time or 0 to exit program");
         Scanner user = new Scanner(System.in);
         int i = user.nextInt();
-        if (i==1){
-            questionOne();
-        }else if (i==2){
-            questionTwo();
+        while (i!=0) {
+            if (i==1) questionOne();
+            else if (i==2) questionTwo();
+            else if (i==3) questionThree();
+            System.out.println("Please enter values between 1-3 for questions or 0 to exit");
+            i = user.nextInt();
         }
 
 
